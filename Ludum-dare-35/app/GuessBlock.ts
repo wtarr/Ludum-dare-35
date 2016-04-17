c4b4c62599f9e349f4552daab0d646ad259fe022
Will <william.projects.2012@gmail.com>
2016-04-17 23:19:24 +0100

    gh-pages setup

--- Ludum-dare-35/app/GuessBlock.ts
+++
@@ -1,8 +0,0 @@
-module LD35 {
-    export class GuessBlock {
-        setGuess: Shape;
-        isSet: boolean;
-
-        spriteRef : Phaser.Sprite;
-    }
-}
--- Ludum-dare-35/app/references.ts
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="../lib/ts/phaser.comments.d.ts"/>
-/// <reference path="game.ts"/>
-/// <reference path="hero.ts"/>
-/// <reference path="guessblock.ts"/>
-/// <reference path="level.ts"/>
-/// <reference path="level1.ts"/>
-/// <reference path="level2.ts"/>
-/// <reference path="level3.ts"/>
-/// <reference path="finished.ts"/>
